bc4f27de3be854df67655f1198bc4cebd68147a0
Jake Jaworski <jakejaworski28@gmail.com>
2022-06-06 16:38:06 -0700

    Revised email criterion
    Changed email to only send on Mondays

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
+import smtplib
+import datetime as dt
+import random
 
-names_list = []
+email = "etest5469@gmail.com"
+password = "12pass34"
 
+# select random quote
+with open(file="quotes.txt") as quotes:
+    quote_list = quotes.readlines()
+    random_quote = random.choice(quote_list)
 
-def create_names_list():
-    final_list = []
-    with open("Input/Names/invited_names.txt") as names:
-        name_list = names.readlines()
-        for name in name_list:
-            final_list.append(name.strip())
-    return final_list
-
-
-
-def extract_text():
-    with open("Input/Letters/starting_letter.txt") as letter:
-        contents = letter.read()
-        return contents
-
-
-def add_name(name, contents):
-    letter_text = contents.replace("[name]", name)
-    return letter_text
-
-
-def create_letter(name, letter_text):
-    with open(f"Output/ReadyToSend/letter_for_{name}.txt", mode="w") as letter:
-        letter.write(letter_text)
-
-
-names_list = create_names_list()
-
-for name in names_list:
-    raw_text = extract_text()
-    letter_text = add_name(name, raw_text)
-    create_letter(name, letter_text)
+# send email if monday
+timestamp = dt.datetime.now()
+if timestamp.weekday() == 0:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(from_addr=email,
+                            to_addrs=email,
+                            msg=f"Subject:Test\n\n{random_quote}")
+        connection.close()
